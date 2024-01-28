2c5d30c5a9816f6f460a8dee4072b4f30368a4f6
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-01-28 23:04:41 +0900

    Rewrite as a Vite project

--- src/components/Main/Translator/LanguageContainer/LanguageSelector/LanguageSelector.tsx
+++ src/components/Main/Translator/LanguageContainer/LanguageSelector/LanguageSelector.tsx
@@ -41,7 +41,6 @@ export default function LanguageSelector({
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
           className={style.icon}
           style={{
             transition: "transform 0.1s",
@@ -51,9 +50,9 @@ export default function LanguageSelector({
           <path
             d="M19 9L12 16L5 9"
             stroke="currentColor"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></path>
         </svg>
       </button>
